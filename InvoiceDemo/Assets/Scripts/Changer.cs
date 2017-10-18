eb62317ffd75353efad5c75f22d356b06018e5d9
jsterling15 <joshua.sterling15@gmail.com>
2017-10-17 22:19:45 -0500

    Added conveyor, robot
    invoices change sprite under lamp, conveyor belt carries, added 2nd generator and workflow cog

---
+++ InvoiceDemo/Assets/Scripts/Changer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changer : MonoBehaviour {
+
+    public Sprite newSkin;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Invoice")
+        {
+           if(collision.gameObject.GetComponent<SpriteRenderer>())
+            {
+                collision.GetComponent<SpriteRenderer>().sprite = newSkin;
+            }
+           else
+            {
+                Debug.Log("No renderer found!");
+            }
+        }
+    }
+
+}
